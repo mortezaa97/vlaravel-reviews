5cd9d7dcea68e2b2f8f457fff316f65aa00196ad
mortezaa <mortezajafari76@gmail.com>
2025-09-27 13:13:37 +0330

    fix:route api middleware issue

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Mortezaa97\Reviews\Http\Controllers\ReviewController;
 
-Route::prefix('api/reviews')->group(function () {
+Route::prefix('api/reviews')->middleware('api')->group(function () {
     Route::get('/', [ReviewController::class, 'index'])->name('reviews.index');
     Route::get('/{review}', [ReviewController::class, 'show'])->name('reviews.show');
     Route::match(['put', 'patch'], '/{review}', [ReviewController::class, 'update'])->middleware('auth:api')->name('reviews.update');
