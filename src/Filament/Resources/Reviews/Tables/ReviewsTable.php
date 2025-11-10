ddbecd716f5e0cdcef7f90db2d696026bea580be
mortezaa <mortezajafari76@gmail.com>
2025-11-11 01:36:50 +0330

    fix: issues

--- src/Filament/Resources/Reviews/Tables/ReviewsTable.php
+++ src/Filament/Resources/Reviews/Tables/ReviewsTable.php
@@ -32,7 +32,7 @@ class ReviewsTable
                 TrashedFilter::make(),
             ])
             ->recordActions([
-                EditAction::make(),
+                EditAction::make()->iconButton()->tooltip('ویرایش'),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
